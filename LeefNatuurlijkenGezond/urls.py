37845eeec42be666c74029efac439dcc8a7729ff
eek2python <eekpython@gmail.com>
2025-12-10 21:29:08 +0100

    update urls.py robots.txt

--- LeefNatuurlijkenGezond/urls.py
+++ LeefNatuurlijkenGezond/urls.py
@@ -21,6 +21,7 @@ from django.contrib import admin
 from django.urls import path, include
 from products import views
 from .sitemap_views import sitemap_xml
+from django.views.generic import TemplateView
 
 urlpatterns = [
    path("admin/", admin.site.urls),
@@ -33,4 +34,5 @@ urlpatterns = [
    path("vershoudcontainers/", views.vershoudcontainers, name="vershoudcontainers"),
    path("blogs/", include("blogs.urls")),
    path("sitemap.xml", sitemap_xml, name="sitemap"),
+   path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
